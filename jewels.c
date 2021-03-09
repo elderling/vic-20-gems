b275e93571d9e85fa90c3d5c672b3824cd73522d
Jonathan Taylor <jtaylor@datafoundry.com>
2021-03-09 13:21:35 -0600

    Add up-down and left-right match tests.

--- jewels.c
+++ jewels.c
@@ -10,17 +10,19 @@ void init_playfield();
 void draw_playfield( unsigned char [PLAYFIELD_X][PLAYFIELD_Y] );
 void init_playfield( unsigned char [PLAYFIELD_X][PLAYFIELD_Y] );
 void clear_groups_of_3_or_more();
-unsigned char clear_jewel( char, char, unsigned char);
+unsigned char must_clear_jewel( char, char, unsigned char);
+unsigned char up_down_match( char x, char y, unsigned char color);
+unsigned char left_right_match( char x, char y, unsigned char color);
 
 unsigned char playfield[PLAYFIELD_X][PLAYFIELD_Y] ={ 
 {'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
-{'a','b','c','d','e', 'f', 'g', 'h'},
+{'b','c','d','e','f', 'g', 'h', 'i'},
+{'c','d','e','f','g', 'h', 'i', 'j'},
+{'d','e','f','g','h', 'i', 'j', 'k'},
+{'e','f','g','h','i', 'j', 'k', 'l'},
+{'f','g','h','i','j', 'k', 'l', 'm'},
+{'g','h','i','j','k', 'l', 'm', 'n'},
+{'h','i','j','k','l', 'm', 'n', 'o'},
 };
 
 unsigned char clear_jewel_tracker[PLAYFIELD_X][PLAYFIELD_Y];
@@ -33,7 +35,7 @@ int main(void) {
 
   clrscr();
   init_playfield(clear_jewel_tracker);
-  draw_playfield( clear_jewel_tracker );
+  draw_playfield( playfield );
   gotoxy(0,19);
   
   cprintf("%d, %d", screen_x, screen_y);
@@ -65,13 +67,39 @@ void clear_groups_of_3_or_more() {
     for (x = 0; x < PLAYFIELD_X; x++) {
       for (y = 0; y < PLAYFIELD_Y; y++) {
         current_jewel_color = playfield[x][y];
-        if ( clear_jewel(x,y,current_jewel_color) ) {
+        if ( must_clear_jewel(x,y,current_jewel_color) ) {
           clear_jewel_tracker[x][y] = 1;
         }
       }
   }
 }
 
-unsigned char clear_jewel( char x, char y, unsigned char color) {
-  return 1;
+unsigned char must_clear_jewel( char x, char y, unsigned char color) {
+  if ( up_down_match( x, y, color) ) {
+    return 1;
+  }
+  if ( left_right_match( x, y, color) ) {
+    return 1;
+  }
+  return 0;
+}
+
+unsigned char up_down_match( char x, char y, unsigned char color) {
+  if ( y == 0 || y == PLAYFIELD_Y - 1 ) {
+    return 0;
+  }
+  if ( playfield[x][y-1] == color && playfield[x][y+1] == color ) {
+    return 1;
+  }
+  return 0;
+}
+
+unsigned char left_right_match( char x, char y, unsigned char color) {
+  if ( x == 0 || x == PLAYFIELD_X - 1 ) {
+    return 0;
+  }
+  if ( playfield[x-1][y] == color && playfield[x+1][y] == color ) {
+    return 1;
+  }
+  return 0;
 }
