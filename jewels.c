6e1e0c99949e65dd815b2b7cd0768c228fe1236a
Jonathan W. Taylor <elderling@gmail.com>
2021-03-13 12:56:00 -0600

    Add checks for double cardinal directions

--- jewels.c
+++ jewels.c
@@ -13,6 +13,10 @@ void clear_groups_of_3_or_more();
 unsigned char must_clear_jewel( char, char, unsigned char);
 unsigned char up_down_match( char x, char y, unsigned char color);
 unsigned char left_right_match( char x, char y, unsigned char color);
+unsigned char double_up_match( char x, char y, unsigned char color);
+unsigned char double_down_match( char x, char y, unsigned char color);
+unsigned char double_left_match( char x, char y, unsigned char color);
+unsigned char double_right_match( char x, char y, unsigned char color);
 
 unsigned char playfield[PLAYFIELD_X][PLAYFIELD_Y] ={ 
 {'a','a','a','d','e', 'f', 'g', 'h'},
@@ -20,9 +24,9 @@ unsigned char playfield[PLAYFIELD_X][PLAYFIELD_Y] ={
 {'c','d','e','f','g', 'h', 'i', 'j'},
 {'d','e','f','g','h', 'i', 'j', 'k'},
 {'e','f','g','h','i', 'j', 'k', 'l'},
-{'f','g','h','i','j', 'k', 'l', 'm'},
+{'f','g','h','i','j', 'l', 'l', 'm'},
 {'g','h','i','j','k', 'l', 'm', 'n'},
-{'h','i','j','k','l', 'm', 'n', 'o'},
+{'h','i','j','k','l', 'l', 'l', 'o'},
 };
 
 unsigned char clear_jewel_tracker[PLAYFIELD_X][PLAYFIELD_Y];
@@ -82,6 +86,21 @@ unsigned char must_clear_jewel( char x, char y, unsigned char color) {
   if ( left_right_match( x, y, color) ) {
     return 1;
   }
+  if (up_down_match( x, y, color) ) {
+    return 1;
+  }
+  if (double_left_match( x, y, color) ) {
+    return 1;
+  }
+  if (double_right_match( x, y, color) ) {
+    return 1;
+  }
+  if (double_up_match( x, y, color) ) {
+    return 1;
+  }
+  if (double_down_match( x, y, color) ) {
+    return 1;
+  }
   return 0;
 }
 
@@ -102,5 +121,51 @@ unsigned char left_right_match( char x, char y, unsigned char color) {
   if ( playfield[x-1][y] == color && playfield[x+1][y] == color ) {
     return 1;
   }
+  return 0;
+}
+
+unsigned char double_up_match( char x, char y, unsigned char color) {
+  if ( y < 2 ) {
+    return 0;
+  }
+  if ( playfield[x][y-1] == color && playfield[x][y-2] == color ) {
+    return 1;
+  }
+
+  return 0;
+}
+
+unsigned char double_down_match( char x, char y, unsigned char color) {
+  if ( y > PLAYFIELD_Y - 3 ) {
+    return 0;
+  }
+
+  if ( playfield[x][y+1] == color && playfield[x][y+2] == color ) {
+    return 1;
+  }
+
+  return 0;
+}
+
+unsigned char double_left_match( char x, char y, unsigned char color) {
+  if ( x < 2 ) {
+    return 0;
+  }
+  if ( playfield[x-1][y] == color && playfield[x-2][y] == color ) {
+    return 1;
+  }
+
+  return 0;
+}
+
+unsigned char double_right_match( char x, char y, unsigned char color) {
+  if ( y > PLAYFIELD_X - 3 ) {
+    return 0;
+  }
+
+  if ( playfield[x+1][y] == color && playfield[x+2][y] == color ) {
+    return 1;
+  }
+
   return 0;
 }
