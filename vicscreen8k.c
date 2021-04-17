36675cadf37241354f0584cf676bb6e3f66fbd5b
Jonathan W. Taylor <elderling@gmail.com>
2021-04-17 15:21:43 -0500

    Print the characters in a grid

--- vicscreen8k.c
+++ vicscreen8k.c
@@ -1,19 +1,22 @@
 #include <vic20.h>
+#include <peekpoke.h>
 
-# define outb(addr,val)        (*(addr) = (val))
-# define inb(addr)             (*(addr))
 # define SCREEN 0x1000      // expanded
 # define COLOR_START 0x9400  // expanded
 # define COLOR_END 0x97FF
 # define SCREEN_WIDTH 22
 # define SCREEN_HEIGHT 23
 # define SCREEN_SIZE SCREEN_WIDTH * SCREEN_HEIGHT
+# define VID_RAM_LAYOUT_REG 0x9005
 
 int main (void)
 {
   unsigned char* screen;
   unsigned char* color;
-  unsigned int x,y;
+  unsigned int y;
+  char i,j;
+  // Switch to capitals + graphics character set
+  POKE(VID_RAM_LAYOUT_REG, 0xc0);
 
   color = (char*) COLOR_START;
   screen = (char*) SCREEN;
@@ -23,9 +26,9 @@ int main (void)
     color[y] = 2;
   }
 
-  for ( y = 1; y = 65; y++) {
-    for( x = 0; x < SCREEN_SIZE; x++ ) {
-      screen[x] = y;
+  for ( i = 0; i < 16; i++) {
+    for ( j = 0; j < 16; j++) {
+      screen[i + j * 22 ] = i + j * 16;
     }
   }
 
