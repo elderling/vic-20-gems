4bb5eb17e2e4bc9b4aee38c66475dca5b2e063fc
Jonathan W. Taylor <elderling@gmail.com>
2021-04-18 06:30:27 -0500

    Tweak colors and change all tiles to diamonds

--- tiles.c
+++ tiles.c
@@ -6,6 +6,7 @@
 # define SCREEN 0x1000      // expanded
 # define COLOR_START 0x9400  // expanded
 # define COLOR_END 0x97FF
+# define SCREEN_AND_BORDER_COLOR 0x900F
 # define SCREEN_WIDTH 22
 # define SCREEN_HEIGHT 23
 # define SCREEN_SIZE SCREEN_WIDTH * SCREEN_HEIGHT
@@ -15,7 +16,8 @@ void draw_tile( char x, char y, char tile );
 
 char tiles[8][4] = {
   0xe9,0xdf,0x5f,0x69,
-  0x55,0x49,0x4a,0x4b,
+  0xe9,0xdf,0x5f,0x69,
+  //0x55,0x49,0x4a,0x4b, // circle
   0xe9,0xdf,0x5f,0x69,
   0xe9,0xdf,0x5f,0x69,
   0xe9,0xdf,0x5f,0x69,
@@ -36,7 +38,7 @@ char tiles[8][4] = {
 };
 
 char tile_colors[8] = {
-  2,2,2,3,4,5,6,7
+  0,1,2,3,4,5,6,7
 };
 
 int main (void)
@@ -45,6 +47,9 @@ int main (void)
 
   // Switch to capitals + graphics character set
   POKE(VID_RAM_LAYOUT_REG, 0xc0);
+  
+  // Screen color to light blue, border to black
+  POKE(SCREEN_AND_BORDER_COLOR, 0xe8);
 
   for( i = 0; i < 16; i = i + 2 ) {
     for( j = 0; j < 16; j = j + 2) {
