edebc249fa4fcc89090efec5cb9f403f7da5e74b
Jonathan W. Taylor <elderling@gmail.com>
2021-05-30 17:49:46 -0500

    Convert number strings to longs

--- seq_cat.c
+++ seq_cat.c
@@ -1,8 +1,13 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <cbm.h>
 
 int main() {
-  unsigned char lfn, open_result, chkin_result, bytes_read, data;
+  unsigned char string_position, lfn, open_result, chkin_result, bytes_read, data;
+  unsigned char number_string[10];
+  unsigned char** endptr;
+  long int the_actual_number;
+  long int the_long_total = 0;
 
   const unsigned char* name = "advent-20-01,s,r";
   printf("'%s'", name);
@@ -20,14 +25,28 @@ int main() {
 
   printf("chkin_result = '%d'", chkin_result);
 
+  number_string[0] = 0;
+  string_position = 0;
   do {
+    
     bytes_read = cbm_read( lfn, &data, 1);
+
     if ( data == 0x0d ) {
-        printf("\n");
+        //printf("\n");
+        number_string[string_position] = 0;
+        // printf("%s\n", number_string);
+        the_actual_number = strtol(number_string, endptr, 0);
+
+        the_long_total += the_actual_number;
+        printf("%ld\n", the_actual_number);
+
+        string_position = 0;
     }
     else {
-      printf("%c", data);
+      //printf("%c", data);
+      number_string[string_position++] = data;
     }
+
   }
   while ( bytes_read > 0 );
 
@@ -35,6 +54,7 @@ int main() {
 
   printf("\n");
   printf("'%s'", name);
+  printf("The Long Total = %ld", the_long_total);
 
   return 0;
 }
