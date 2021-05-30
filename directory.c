1a432294d5ede97e87beb579c6206c1f9161a966
Jonathan W. Taylor <elderling@gmail.com>
2021-05-30 16:52:20 -0500

    Read SEQ file and print hex values

--- directory.c
+++ directory.c
@@ -2,21 +2,33 @@
 #include <cbm.h>
 
 int main() {
-  char i,n,lfn, open_result, chkin_result, data_char, foo, data;
+  unsigned char i,lfn, open_result, chkin_result, foo, data;
 
-  const char* name = "$";
+  const unsigned char* name = "advent-20-01,s,r";
+  printf("'%s'", name);
   lfn = 1;
 
   open_result = cbm_open( lfn, 8, 0, name );
 
+  if ( open_result != 0 ) {
+    printf("Error: %d", open_result);
+    cbm_k_close( lfn );
+    return -1;
+  }
+
   chkin_result = cbm_k_chkin( lfn );
 
+  printf("chkin_result = '%d'", chkin_result);
+
   for ( i = 0; i < 254; i++ ) {
-    foo = cbm_read( 1, &data, 1);
-    printf("%c,", data);
+    foo = cbm_read( lfn, &data, 1);
+    printf("%x,", data);
   }
 
   cbm_k_close( lfn );
+
+  printf("\n");
+  printf("'%s'", name);
 
   return 0;
 }
