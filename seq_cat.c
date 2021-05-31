99119088fdc6a0e76f4822fd345a48cb4d887e9b
Jonathan W. Taylor <elderling@gmail.com>
2021-05-31 07:56:26 -0500

    Stop reading extra long

--- seq_cat.c
+++ seq_cat.c
@@ -31,20 +31,22 @@ int main() {
     
     bytes_read = cbm_read( lfn, &data, 1);
 
-    if ( data == 0x0d ) {
-        //printf("\n");
-        number_string[string_position] = 0;
-        // printf("%s\n", number_string);
-        the_actual_number = strtol(number_string, endptr, 0);
+    if ( bytes_read ) {
+      if ( data == 0x0d ) {
+          //printf("\n");
+          number_string[string_position] = 0;
+          // printf("%s\n", number_string);
+          the_actual_number = strtol(number_string, endptr, 0);
 
-        the_long_total += the_actual_number;
-        printf("%ld\n", the_actual_number);
+          the_long_total += the_actual_number;
+          printf("%ld\n", the_actual_number);
 
-        string_position = 0;
-    }
-    else {
-      //printf("%c", data);
-      number_string[string_position++] = data;
+          string_position = 0;
+      }
+      else {
+        //printf("%c", data);
+        number_string[string_position++] = data;
+      }
     }
 
   }
