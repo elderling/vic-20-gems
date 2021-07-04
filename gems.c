131d629f6aa6e7ca8239a36c9ab141085f4b054c
Jonathan W. Taylor <elderling@gmail.com>
2021-07-04 10:17:12 -0500

    Stub in display logic

--- gems.c
+++ gems.c
@@ -1,11 +1,25 @@
 #include <vic20.h>
 #include <conio.h>
 
+#define PLAYFIELD_X 8
+#define PLAYFIELD_Y 8
+
 void initialize_display();
+void initialize_game_state();
+void render_display();
+
+struct coordinate {
+  char x;
+  char y;
+};
+
+struct coordinate game_cursor;
 
 int main() {
 
   initialize_display();
+  initialize_game_state();
+  render_display();
 
   return 0;
 }
@@ -17,4 +31,17 @@ void initialize_display() {
   cursor(1);
 
   return;
+}
+
+void initialize_game_state() {
+  game_cursor.x = 0;
+  game_cursor.y = 0;
+
+  return;
+}
+
+void render_display() {
+  gotoxy(0, PLAYFIELD_Y);
+  cprintf("x=%2d,y=%2d", game_cursor.x, game_cursor.y);
+  gotoxy( game_cursor.x, game_cursor.y );
 }
